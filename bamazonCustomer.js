33e2640cd7161fb798de569c4dbfc42fc0cbba1b
Andrew Bayles <baylesweb@gmail.com>
2019-05-17 17:06:02 -0500

    Add Inquirer purchase prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,23 +12,74 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if(err) throw err;
     console.log(`MySQL connect on ${connection.threadId}`);
+
     displayAllItems();
+    purchasePrompt();
+
+    connection.end;
 });
 
 function displayAllItems() {
 
-    console.log("All Available Products\n");
+    console.log("All Available Products");
     console.log("----------------------\n");
 
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         Object.keys(res).forEach(function(key) {
             var row = res[key];
-            console.log("ID: " + row.id + " | " + row.product_name + " | Price: " + row.price + "\n");
+            console.log("ID: " + row.id + " | " + row.product_name + " | Price: " + row.price);
         });    
     });
 
 }
 
 
-connection.end;
+
+function purchasePrompt() {
+
+    var itemIds = [];
+
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        Object.keys(res).forEach(function(key) {
+            var row = res[key];
+            itemIds.push(row.id);
+        });    
+    });
+
+    inquirer.prompt([
+        {
+            name: "purchaseId",
+            type: "input",
+            message: "What is the product ID of the item you want to buy?"
+        },
+        {
+            name: "purchaseQuantity",
+            type: "input",
+            message: "How many units would you like to buy?"
+        }
+    ]).then(function(answer) {
+
+        if (itemIds.includes(answer.purchaseId)) {
+
+
+            
+        
+        } else {
+            console.log("Sorry, that's not a valid product ID!");
+            purchasePrompt();
+        }
+
+    });
+
+}
+
+
+
+
+function fulfillOrder() {
+
+}
+
+
