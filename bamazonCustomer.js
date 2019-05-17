8b8d3c281179bf5c485bd2a75a837db22bb6e0a5
Andrew Bayles <baylesweb@gmail.com>
2019-05-17 16:01:35 -0500

    Add Display Items Functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,34 @@
-var Store = require("./lib/Store");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-// Initialize a new Store object
-var store = new Store();
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'bamazon'
+});
 
-// Start store
-store.start();
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log(`MySQL connect on ${connection.threadId}`);
+    displayAllItems();
+});
+
+function displayAllItems() {
+
+    console.log("All Available Products\n");
+    console.log("----------------------\n");
+
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        Object.keys(res).forEach(function(key) {
+            var row = res[key];
+            console.log("ID: " + row.id + " | " + row.product_name + " | Price: " + row.price + "\n");
+        });    
+    });
+
+}
+
+
+connection.end;
